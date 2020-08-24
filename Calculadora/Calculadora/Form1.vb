a4e8c03d02b03e49e496462463d3adc40e086fb9
Geovanni Vasquez <USIS023619@ugb.edu.sv>
2020-08-23 21:18:15 -0600

    CREACION DE RAMA

--- Calculadora/Calculadora/Form1.vb
+++ Calculadora/Calculadora/Form1.vb
@@ -1,169 +1,9 @@
 ﻿Public Class Form1
-    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles optsuma.CheckedChanged
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs)
 
     End Sub
 
-    Private Sub cal_Click(sender As Object, e As EventArgs) Handles cal.Click
-
-
-
-
-
-
-        'SE INICIALIZAN PRIMERAMENTE LAS VARIABLES.
-        Dim num1, num2 As Double
-        '-----------------------------------------------------------------------------------------
-
-
-        'DEFINIMOS CADA UNA DE ELLAS.
-        num1 = txtnum1.Text
-        num2 = txtnum2.Text
-        '----------------------------------------------------------------------------------------------
-
-
-        'UTILIZAMOS UNA CONDICION PARA SELECCIONAR EL TIPO DE OPERACION QUE QUERAMOS REALIZAR.
-        If optsuma.Checked Then
-            result.Text = num1 + num2
-        End If
-        '---------------------------------------------------------------------------------------------
-
-        If optresta.Checked Then
-            result.Text = num1 - num2
-        End If
-
-
-
-
-        If optmultiplicacion.Checked Then
-            result.Text = num1 * num2
-        End If
-
-
-
-        If optdivision.Checked Then
-            result.Text = num1 / num2
-        End If
-
-
-        'PARA SACAR EL PORCENTAJE LA OPERACIO QUE TENEMOS QUE REALIZAR ES LA MULTIPLICACION DE DOS NUMEROS
-        'ENTRE 100  
-        If optporcen.Checked Then
-            result.Text = num1 * num2 / 100
-        End If
-        '-------------------------------------------------------------------------------------------------------
-
-        If optelevacion.Checked Then
-            result.Text = num1 ^ num2
-        End If
-
-
-
-
-
-        If optmodis.Checked Then
-            result.Text = num1 Mod num2
-        End If
-
-
-
-
-        '----------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-        Select Case cboOperaciones.SelectedIndex
-            Case 0
-                result.Text = num1 + num2
-
-        End Select
-
-
-
-        Select Case cboOperaciones.SelectedIndex
-            Case 1
-                result.Text = num1 - num2
-
-        End Select
-
-
-
-
-
-        Select Case cboOperaciones.SelectedIndex
-            Case 2
-                result.Text = num1 * num2
-
-        End Select
-
-
-
-        Select Case cboOperaciones.SelectedIndex
-            Case 3
-                result.Text = num1 / num2
-        End Select
-
-
-
-        Select Case cboOperaciones.SelectedIndex
-            Case 4
-                result.Text = num1 * num2 / 100
-        End Select
-
-
-
-        Select Case cboOperaciones.SelectedIndex
-            Case 5
-                result.Text = num1 ^ num2
-        End Select
-
-
-
-        Select Case cboOperaciones.SelectedIndex
-            Case 6
-                result.Text = num1 Mod num2
-
-        End Select
-
-
-
-
-
-
-
-
-
-        '------------------------------------------------------------------------------------------------▽
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        'TENGAN UN LINDO DIA, GRACIAS POR VER MI CODIGO.  ＾▽＾
-
-
-
-
-        '-------------------------------------------------------------------------------------------------▽
-
-
+    Private Sub cal_Click(sender As Object, e As EventArgs) 
 
 
 
@@ -176,11 +16,11 @@
 
     End Sub
 
-    Private Sub RadioButton1_CheckedChanged_1(sender As Object, e As EventArgs) Handles optmodis.CheckedChanged
+    Private Sub RadioButton1_CheckedChanged_1(sender As Object, e As EventArgs)
 
     End Sub
 
-    Private Sub comb_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboOperaciones.SelectedIndexChanged
+    Private Sub comb_SelectedIndexChanged(sender As Object, e As EventArgs)
 
     End Sub
 End Class
